1b41bb74b71316fe313e918b6bfe15822dcefea2
Lian Hoy Lee <cruz@zyk3.net>
2017-10-27 11:25:37 +1300

    feat(cfg): add warning when environment configuration is not found

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const path = require('path');
+const { assert } = require('chai');
 
 const { logger } = require('./logging');
 const configurator = require('./utilities/configurator');
 
 class Environment {
     constructor(name) {
+        assert.exists(name);
+
         this.name = name;
         this.log = logger(this.name);
         this.app = new Koa();
         this.router = new Router();
 
         const callerPath = path.dirname(module.parent.filename);
-        this.cfg = configurator(callerPath);
+        this.cfg = configurator(this, callerPath);
     }
 
     get port() {
--- src/utilities/configurator.js
+++ src/utilities/configurator.js
@@ -1,14 +1,24 @@
 const path = require('path');
 
-function loadConfiguration(cfgPath) {
+function loadConfiguration(env, cfgPath) {
     const cfg = require(path.join(cfgPath, 'config.js'));
 
-    const env = process.env.NODE_ENV;
-    if (!env) {
+    const envName = process.env.NODE_ENV;
+    if (!envName) {
         return cfg;
     }
 
-    const envCfg = require(path.join(cfgPath, `config.${env}.js`));
+    const envCfgName = `config.${envName}.js`;
+    let envCfg = {};
+    try {
+        envCfg = require(path.join(cfgPath, envCfgName));
+    } catch (err) {
+        if (err.code !== 'MODULE_NOT_FOUND') {
+            throw err;
+        }
+        env.log.warn(`Environment configuration ${envCfgName} not found`);
+    }
+
     return Object.assign({}, cfg, envCfg);
 }
 
