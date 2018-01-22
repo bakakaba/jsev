4f105ae46d907b02cd0547e5ce4a4bcb3371749c
Lee Lian Hoy <cruz@zyk3.net>
2018-01-22 13:26:18 +1300

    fix(jwt): fix bypass for public routes

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -2,10 +2,10 @@ module.exports = {
     func: (ctx, next) => {
         // By convention, we don't authenticate if the route is public and there is no authorization header
         if (ctx.url.match(/^\/public/) && !ctx.headers.authorization) {
-            return;
+            return next();
         }
 
-        ctx.services.jwt(ctx, next);
+        return ctx.services.jwt(ctx, next);
     },
     rank: 12,
 };
