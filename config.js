4749d74b3098795f82d9ae298fd04f21523beca5
Lee Lian Hoy <cruz@zyk3.net>
2018-05-31 16:34:06 +1200

    task(config): remove sample tokens from config

--- config.js
+++ config.js
@@ -9,15 +9,8 @@ module.exports = {
             showTime: false,
         },
         level: 'trace',
-        sumologic: {
-            collector: 'ZaVnC4dhaV20qZ8mnTh_8Nw5tFl6r2shgaMAvpsxDpe2_lSGmb8DXdvwsoRLI7B6LBOTi5m2z4aX1YyTTRZFZk5IY6wXrN_hOq7ybx0L_1idodlEl2tmBg==',
-            endpoint: 'https://collectors.au.sumologic.com/receiver/v1/http/',
-        },
     },
     port: 5000,
-    raygun: {
-        apiKey: '2o2ZbCaFpbHLry4mJfZtaQ==',
-    },
     requestLogger: {
         logLevelOverrides: {
             debug: 'trace',
