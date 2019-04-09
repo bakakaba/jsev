cdb81863a61da8b40ae6160ea2b5964419e2476f
Lee Lian Hoy <cruz@zyk3.net>
2019-04-09 18:45:37 +1200

    feat(ts): update index.js to typescript

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+// import Koa from 'koa';
+// const Router = require('koa-router');
+// const path = require('path');
+// const { assert } = require('chai');
+
+// const { Logger } = require('./logging');
+// const { defaultMiddlewares, applyMiddlewares } = require('./middlewares');
+// const utilities = require('./utilities');
+// const errors = require('./errors');
+
+export default class Environment {
+    // constructor(name) {
+    //     assert.exists(name);
+
+    //     this.name = name;
+    //     this.app = new Koa();
+    //     this.app.context.env = this;
+
+    //     const callerPath = path.dirname(module.parent.filename);
+    //     this.cfg = utilities.configurator.loadConfiguration(callerPath);
+
+    //     this.log = new Logger(this);
+    //     this.middlewares = defaultMiddlewares;
+
+    //     this.router = new Router();
+    //     this.app.on('error', (err) => {
+    //         this.log.fatal(err);
+
+    //         if (this.raygun) {
+    //             // eslint-disable-next-line no-empty-function
+    //             this.raygun.send(err, {}, () => {}, this.app.context.request, ['fatal']);
+    //         }
+    //     });
+    // }
+
+    // get port() {
+    //     if (!this.cfg.port) {
+    //         throw new Error('Port has not been specified in the configuration.');
+    //     }
+
+    //     return this.cfg.port;
+    // }
+
+    // run() {
+    //     applyMiddlewares(this);
+
+    //     this.app.listen(this.port);
+    //     this.log.info(`Listening on port ${this.port} (${this.cfg.env})`);
+    // }
+}
+
+// module.exports = {
+//     Environment,
+//     errors,
+//     utilities,
+// };
